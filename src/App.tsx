4727a3bc580379aa4b2933135c77ed659eb0480d
tolluset <dlqud19@gmail.com>
2022-02-03 00:54:47 +0900

    feat: parse the discord code from query params

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useSearchParams } from "react-router-dom";
 
 function App() {
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get("code");
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Use code:
+          <br />
+          <code>{code}</code>
+          <br />
+          to finish the authorizing.
         </p>
         <a
           className="App-link"
